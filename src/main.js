79679398681b2eb63aea0c3a42cb73cba8f948ae
Artem Panov <43380883+panovaartem@users.noreply.github.com>
2024-09-11 22:13:40 +0500

    fix bugs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import SvgVue from 'svg-vue';
 import VueLodash from 'vue-lodash';
 import vSelect from 'vue-select';
 import vueNumeralFilterInstaller from 'vue-numeral-filter';
@@ -8,9 +7,12 @@ import lodash from 'lodash';
 import router from './router/router';
 import KProgress from 'k-progress';
 
+import './assets/css/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
 Vue.config.productionTip = false
 
-Vue.use(SvgVue);
 Vue.component('v-select', vSelect);
 Vue.use(vueNumeralFilterInstaller, { locale: 'en' });
 Vue.use(VueLodash, { name: 'custom', lodash: lodash });
